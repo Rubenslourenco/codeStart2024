ba3c749ec2ab2251e95f04cefb6669f4adb80e08
Rubens Alves <alvesrubens926@gmail.com>
2024-11-15 23:20:32 -0300

    Eventos em Dom 3

--- DOM/main.js
+++ DOM/main.js
@@ -1,6 +1,7 @@
 //const addUserText = document.getElementById("add-user");
 //addUserText.innerText = "Adicionar Usuário";
 const items = document.querySelector(".items");
+//const button = document.querySelector(".btn");
 
 //Remover item da lista
 //items.remove();
@@ -9,4 +10,28 @@ const items = document.querySelector(".items");
 //items.firstElementChild.remove();
 //items.lastElementChild.remove();
 
-items.children[1].innerText = "novo texto";
+//Alterar o texto do segundo item da lista
+//items.children[1].innerText = "novo texto";
+
+//Alterar a cor de fundo do botão
+//button.style.background = "red";
+
+const submitButton = document.querySelector("#submit-button");
+const myForm = document.querySelector("#my-form");
+const nameInput = document.querySelector("#name");
+const emailInput = document.querySelector("#email");
+
+submitButton.addEventListener("click", function (e) {
+  e.preventDefault();
+
+  const name = nameInput.value;
+  const email = emailInput.value;
+
+  if (name === "" || email === "") {
+    return alert("Preencha todos os campos");
+  }
+
+  myForm.style.background = "red";
+  items.firstElementChild.textContent = name;
+  items.children[1].textContent = email;
+});
