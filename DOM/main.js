fea60b98394d20a9663b41e10299191998e0c874
Rubens Alves <alvesrubens926@gmail.com>
2024-11-16 14:55:44 -0300

    Finalizando Dom

--- DOM/main.js
+++ DOM/main.js
@@ -1,37 +1,30 @@
-//const addUserText = document.getElementById("add-user");
-//addUserText.innerText = "Adicionar Usuário";
-const items = document.querySelector(".items");
-//const button = document.querySelector(".btn");
-
-//Remover item da lista
-//items.remove();
-
-//Remover o primeiro e o último item da lista
-//items.firstElementChild.remove();
-//items.lastElementChild.remove();
-
-//Alterar o texto do segundo item da lista
-//items.children[1].innerText = "novo texto";
-
-//Alterar a cor de fundo do botão
-//button.style.background = "red";
-
-const submitButton = document.querySelector("#submit-button");
-const myForm = document.querySelector("#my-form");
 const nameInput = document.querySelector("#name");
 const emailInput = document.querySelector("#email");
+const submitButton = document.querySelector("#submit-button");
+
+const erroMessage = document.querySelector(".msg");
+const items = document.querySelector(".items");
 
 submitButton.addEventListener("click", function (e) {
   e.preventDefault();
 
-  const name = nameInput.value;
-  const email = emailInput.value;
+  const nameValue = nameInput.value;
+  const emailValue = emailInput.value;
 
-  if (name === "" || email === "") {
-    return alert("Preencha todos os campos");
+  if (nameValue === "" || emailValue === "") {
+    erroMessage.textContent = "Preencha todos os campos";
+    erroMessage.classList = "error";
+
+    setTimeout(() => {
+      erroMessage.textContent = "";
+      erroMessage.classList = "";
+    }, 3000);
+
+    return;
   }
+  const li = document.createElement("li");
+  li.classList = "item";
+  li.innerHTML = `Nome: ${nameValue} </br> Email: ${emailValue}`;
 
-  myForm.style.background = "red";
-  items.firstElementChild.textContent = name;
-  items.children[1].textContent = email;
+  items.appendChild(li);
 });
